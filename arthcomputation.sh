4fe7756501af7a376a5aac69e40a825417c5fb1f
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-10 04:06:08 +0530

    store in dictionary

--- arthcomputation.sh
+++ arthcomputation.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
+declare -A result
 read -p "Enter A value: " a
 read -p "Enter B value: " b
 read -p "Enter C value: " c
@@ -10,3 +11,12 @@ uc4=$(($c+$a/$b))
 echo "c+a/b is " $uc4
 uc5=$(($a%$b+$c))
 echo "a%b+c is " $uc5
+
+counter=0;
+
+result[$((counter++))]="$uc2"
+result[$((counter++))]="$uc3"
+result[$((counter++))]="$uc4"
+result[$((counter++))]="$uc5"
+
+echo ${result[@]}

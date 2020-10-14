033a557f769d8e29bb7d897f96f230d839a38d42
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-14 21:03:20 +0530

    conflict resolved

--- arthcomputation.sh
+++ arthcomputation.sh
@@ -21,30 +21,15 @@ result[$((counter++))]="$uc5"
 echo ${result[@]}
 for (( i=0; i<5; i++ ))
 do
-<<<<<<< HEAD
-<<<<<<< HEAD
-	arr[i]=${result[$(($i-1))]}
-done
-echo ${arr[@]}
-=======
         arr[i]=${result[$(($i-1))]}
 done
 echo ${arr[@]}
-=======
-        arr[i]=${result[$(($i-1))]}
-done
-echo ${arr[@]}
->>>>>>> ascendingsortUC9
 
 for (( k=0; k<3; k++ ))
 do
 	for (( j=k+1; j<3; j++ ))
 	do
-<<<<<<< HEAD
-		if [[ ${arr[$k]} -lt ${arr[$j]} ]]
-=======
 		if [[ ${arr[$k]} -gt ${arr[$j]} ]]
->>>>>>> ascendingsortUC9
 		then
 			temp=${arr[$k]}
 			arr[$k]=${arr[$j]}
@@ -54,7 +39,4 @@ do
 done
 
 echo "${arr[@]}"
-<<<<<<< HEAD
->>>>>>> descendingsortUC8
-=======
->>>>>>> ascendingsortUC9
+
